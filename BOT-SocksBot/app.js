a0b2d2ab51e24401962d07197cfa2ccea819e5d0
MeemDes <65924819+MeemDes@users.noreply.github.com>
2020-05-26 00:43:21 +0300

    Update app.js

--- BOT-SocksBot/app.js
+++ BOT-SocksBot/app.js
@@ -4,4 +4,4 @@ console.log("Iniciando bot :)");
 
 
 // Args: (url, optionID, protection, socksFilePath, amount)
-botPoll('https://www.strawpoll.me/17311284', 1, true, join(__dirname, 'socks.txt'));
+botPoll('https://strawpoll.com/kdk39w5y', 1, true, join(__dirname, 'socks.txt'));
